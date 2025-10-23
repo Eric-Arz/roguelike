6464b6a34234a28998a6bc497ea81723fec8e2bf
ericarz <eric.arzberger@web.de>
2025-10-23 21:52:05 +0200

    added sdl

--- src/main.c
+++ src/main.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
 
+#include <SDL3/SDL.h>
+
 int main(int argc, const char *argv[]) {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_Init failed: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Quit();
+
     return EXIT_SUCCESS;
 }
